18219e79ffa8f570ecc6d4f6db9dbd0ebb276e39
1OneZero <1onenzero0@gmail.com>
2022-01-18 17:32:26 +0530

    update

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app  = express();
+const PORT = process.env.PORT || 2000;
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+app.use(express.json());
+
+
+io.on('connection',(socket)=>{
+    console.log("user connected to scoket id: ",socket.id)
+});
+
+server.listen(PORT,"0.0.0.0",()=>{
+    console.log("server started");
+})
